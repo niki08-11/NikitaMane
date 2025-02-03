28d5c0664812cc95967c61e834650f2e60631848
niki <nm0049720@gmail.com>
2025-02-03 10:50:10 +0530

    my first python code

---
+++ Hello.py
@@ -0,0 +1,11 @@
+# Example: calculate factorial of 5
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+n = 5
+result = factorial(n)
+print(f"The factorial of {n} is {result}")
+
